36f0192a282b074c03cab117e9d70716a0448423
Alexandre GAREL <alex@sd3249.(none)>
2012-01-09 10:49:30 +0100

    avoid traceback for a bad encoding, use 'replace'

--- helpdesk/management/commands/get_email.py
+++ helpdesk/management/commands/get_email.py
@@ -137,7 +137,7 @@ def decodeUnknown(charset, string):
             return string.decode('utf-8')
         except:
             return string.decode('iso8859-1')
-    return unicode(string, charset)
+    return unicode(string, charset, 'replace')
 
 def decode_mail_headers(string):
     decoded = decode_header(string)
